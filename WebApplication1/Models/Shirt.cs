ec73287adcd6c6d90acf96000554e557faba7fca
unknown <vince.l08816@gmail.com>
2023-12-13 22:34:05 -0500

    found a bug

--- WebApplication1/Models/Shirt.cs
+++ WebApplication1/Models/Shirt.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using WebApplication1.Models.Validations;
 
 // when a type is required, then you can use the [Required] attribute
 // otherwise you can make the variable nullable
@@ -7,13 +8,15 @@ namespace WebApplication1.Models
 {
     public class Shirt
     {
-        [Required]
+        [Required]  
         public int Id { get; set; }
         [Required]
         public string? Color { get; set; }
+        [Shirt_EnsureCorrectSizing]
         public int Size { get; set; }
         public string? Type { get; set; }
         public double Price { get; set; }
+        public string? Gender { get; set; }
     }
 }
 
---
+++ WebApplication1/Models/Validations/Shirt_EnsureCorrectSizingAttribute.cs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.Validations
+{
+    public class Shirt_EnsureCorrectSizingAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var shirt = validationContext.ObjectInstance as Shirt;
+
+            if (shirt != null && !string.IsNullOrWhiteSpace(shirt.Gender))
+            {
+                if (shirt.Gender.Equals("men", StringComparison.OrdinalIgnoreCase) && shirt.Size < 8) 
+                {
+                    return new ValidationResult("For men's shirt, the size must be greater than or equal to 8");
+                }
+                else if (shirt.Gender.Equals("women", StringComparison.OrdinalIgnoreCase) && shirt.Size < 6)
+                {
+                    return new ValidationResult("For women's shirt, the size must be greater than or equal to 6");
+
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
